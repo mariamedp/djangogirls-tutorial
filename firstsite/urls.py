f859485984429845eff8e0921957905becf2b389
María Medina <mariamedp@gmail.com>
2018-08-31 19:15:59 +0200

    Authentication

--- firstsite/urls.py
+++ firstsite/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.urls import path, include
 from django.contrib import admin
 
+from django.contrib.auth import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', views.login, name='login'),
+    path('accounts/logout/', views.logout, name='logout', kwargs={'next_page': '/'}),
     path('', include('blog.urls')),
 ]
